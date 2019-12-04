3b2985543830386a9d81345719504b3f7b470ebd
jawnhoang <54566354+jawnhoang@users.noreply.github.com>
2019-12-04 15:41:15 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_socketio import SocketIO, send, join_room, leave_room
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -17,21 +16,6 @@ login = LoginManager(app)
 migrate = Migrate(app,db)
 manager = Manager(app)
 manager.add_command('db',MigrateCommand)
-
-socketio = SocketIO(app)
-
-
-@socketio.on('join')
-def on_join(data):
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
-
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
 
 
 # right side is the function that's called to login users
