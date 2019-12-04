70607c1c2be33550a037769e82e6edb66fd59ee6
Dai Le <dai.n.le@sjsu.edu>
2019-12-03 20:24:12 -0800

    Add files via upload

--- tests/users_test.py
+++ tests/users_test.py
@@ -1,32 +1,21 @@
 import pytest
 from app.models import User
 
+
 @pytest.fixture(scope='module')
 def new_user():
-    user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
+    user = User(username='daile', email='patkennedy79@gmail.com')
     return user
 
 
 def test_new_user(new_user):
+    assert new_user.username == 'daile'
     assert new_user.email == 'patkennedy79@gmail.com'
-    assert new_user.password_hash != 'FlaskIsAwesome'
-    assert not new_user.authenticated
-    assert new_user.role == 'user'
 
 
-def test_setting_password(new_user):
-    new_user.set_password('MyNewPassword')
-    assert new_user.hashed_password != 'MyNewPassword'
-    assert new_user.is_correct_password('MyNewPassword')
-    assert not new_user.is_correct_password('MyNewPassword2')
-    assert not new_user.is_correct_password('FlaskIsAwesome')
 
 
-def test_check_password(new_user):
-    new_user.check_password("MyNewPassword")
-    assert new_user.check_password != 'MyNewPassword'
-    assert new_user.is_correct_check_password('MyNewPassword')
-    assert not new_user.is_correct_check_password('MyNewPassword2')
-    assert not new_user.is_correct_check_password('FlaskIsAwesome')
+
+
 
 
