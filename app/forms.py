eb6ff2cdee2ca74a794de1391c821728297f83d2
jawnhoang <54566354+jawnhoang@users.noreply.github.com>
2019-12-06 15:00:41 -0800

    fixed same reg entry crash

--- app/forms.py
+++ app/forms.py
@@ -21,21 +21,23 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     course = StringField('What are you studying?', validators=[DataRequired()])
     submit = SubmitField('Register')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username taken. Please use a different name.')
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email taken. Please use a different email.')
+    
+    
 class CourseForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     student_courses = StringField('Courses interested in', validators=[DataRequired()])
     submit = SubmitField('Search')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
 
 class profileForm(FlaskForm):
     name = StringField('Update Username', validators=[DataRequired()])
